ae53157eb0d05fec68c4823c200e35b81ea9bab5
KhaiVox <khaiduchoa@gmail.com>
2022-08-15 22:44:47 +0700

    login/register

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+const AccountModel = require('./models/account')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// Home
+// chỉ phương thức GET mới hiện view
+app.get('/', (req, res, next) => {
+    res.json('Home')
+})
+
+// Register
+app.post('/register', (req, res, next) => {
+    var username = req.body.username
+    var password = req.body.password
+
+    AccountModel.findOne({
+        username: username,
+    })
+        .then((data) => {
+            if (data) {
+                res.json('User này đã tồn tại!')
+            } else {
+                AccountModel.create({
+                    username: username,
+                    password: password,
+                }).then((data) => {
+                    res.json('Tạo tài khoản thành công!')
+                })
+            }
+        })
+        .catch((err) => {
+            res.status(500).json('Tạo tài khoản thất bại!')
+        })
+})
+
+// Login
+app.post('/login', (req, res, next) => {
+    var username = req.body.username
+    var password = req.body.password
+
+    AccountModel.findOne({
+        username: username,
+        password: password,
+    })
+        .then((data) => {
+            if (data) {
+                res.json('Đăng nhập thành công!')
+            } else {
+                res.status(500).json('Tài khoản hoặc mật khẩu chưa chính xác!')
+            }
+        })
+        .catch((err) => {
+            res.status(500).json('Đã xảy ra lỗi!')
+        })
+})
+
+app.listen(5000, () => {
+    console.log('Connect successfully!!!')
+})
